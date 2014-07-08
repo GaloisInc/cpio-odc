738c1fe30717c1a263758e978fad5f62f9f97533
James Bielman <jamesjb@galois.com>
2014-07-08 14:03:26 -0700

    Add license information to source file headers.

--- src/cpio.ml
+++ src/cpio.ml
@@ -4,6 +4,9 @@
  * Copyright (C) 2014, Galois, Inc.
  * All Rights Reserved.
  *
+ * Released under the BSD3 license.  See the file "LICENSE"
+ * for details.
+ *
  * Written by James Bielman <jamesjb@galois.com>, 20 May 2014
  *)
 
